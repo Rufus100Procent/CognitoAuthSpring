d8278e3e80f64c0d3b71547e9b9e3b333e9d52be
Stykle <StykleBlender@duck.com>
2023-12-08 13:06:10 +0100

    testing braanch

--- src/main/java/es/wacoco/CognitoAuthSpring/error/EmailExistsException.java
+++ src/main/java/es/wacoco/CognitoAuthSpring/error/EmailExistsException.java
@@ -1,5 +1,7 @@
 package es.wacoco.CognitoAuthSpring.error;
 
+
+//custom Exception
 public class EmailExistsException extends RuntimeException {
     public EmailExistsException(String message) {
         super(message);
