03045f3ce1a4db6ff7ec39556af87ad36925a26a
Stykle <StykleBlender@duck.com>
2023-12-12 13:36:53 +0100

    update

--- src/main/java/es/wacoco/CognitoAuthSpring/API/CamelRestController.java
+++ src/main/java/es/wacoco/CognitoAuthSpring/API/CamelRestController.java
@@ -1,21 +1,31 @@
 package es.wacoco.CognitoAuthSpring.API;
 
 import es.wacoco.CognitoAuthSpring.Service.CamelServiceClass;
+import es.wacoco.CognitoAuthSpring.Service.CognitoAuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CamelRestController {
+    private static final Logger logger = LoggerFactory.getLogger(CamelRestController.class);
+
     private final CamelServiceClass camelServiceClass;
+    private final CognitoAuthService cognitoAuthService;
 
     @Autowired
-    public CamelRestController(CamelServiceClass camelServiceClass) {
+    public CamelRestController(CamelServiceClass camelServiceClass, CognitoAuthService cognitoAuthService) {
         this.camelServiceClass = camelServiceClass;
+        this.cognitoAuthService = cognitoAuthService;
     }
 
     @GetMapping("/startRoute")
-    public String startRoute() {
-        return camelServiceClass.startRoute();
+    public String startRoute(HttpServletRequest request) {
+        String username = cognitoAuthService.getCurrentLoggedInUsername(request);
+        return camelServiceClass.startRoute(username);
     }
+
 }
